b66f36eee36415b24ff8a8acb3830a9f9261d95f
Richu Jain <richujain1995@gmail.com>
2020-10-06 14:01:20 -0400

    Designed ProfileActivity and added custom toolbar to ProfileActivity and PaymentActivity.

--- app/src/main/java/com/example/delfoodiepassenger/PaymentActivity.java
+++ app/src/main/java/com/example/delfoodiepassenger/PaymentActivity.java
@@ -1,8 +1,11 @@
 package com.example.delfoodiepassenger;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class PaymentActivity extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class PaymentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
+        init();
+    }
+    private void init() {
+        Toolbar toolbar = findViewById(R.id.toolbarPayment);
+        toolbar.bringToFront();
+        toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_baseline_arrow_back_24));
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(PaymentActivity.this,MainActivity.class));
+        finish();
     }
 }
--- app/src/main/java/com/example/delfoodiepassenger/ProfileActivity.java
+++ app/src/main/java/com/example/delfoodiepassenger/ProfileActivity.java
@@ -3,6 +3,7 @@ package com.example.delfoodiepassenger;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -12,7 +13,26 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
+        init();
     }
 
+    private void init() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.bringToFront();
+        toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_baseline_arrow_back_24));
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
 
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(ProfileActivity.this,MainActivity.class));
+        finish();
+    }
 }
