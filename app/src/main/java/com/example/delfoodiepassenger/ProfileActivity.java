95688df77535fb44ca711dfd17db0760793b0cfd
Richu Jain <richujain1995@gmail.com>
2020-10-13 13:21:29 -0400

    Realm Added to the project and initialized some components in ProfileActivity.

--- app/src/main/java/com/example/delfoodiepassenger/ProfileActivity.java
+++ app/src/main/java/com/example/delfoodiepassenger/ProfileActivity.java
@@ -6,9 +6,12 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ProfileActivity extends AppCompatActivity {
-
+    EditText name;
+    Button saveProfileDetails;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +29,14 @@ public class ProfileActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 onBackPressed();
+            }
+        });
+        name = findViewById(R.id.name);
+        saveProfileDetails = findViewById(R.id.saveProfileDetails);
+        saveProfileDetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
         });
     }
