58dc7414943a3a5e1998cb1c8c7df9148dac2b7a
Richu Jain <richujain1995@gmail.com>
2020-10-21 08:45:59 -0400

    Added Functionality to the pincodeSearchButton in MainActivity.java.

---
+++ app/src/main/java/com/example/delfoodiepassenger/RestaurantsNearMe.java
@@ -0,0 +1,14 @@
+package com.example.delfoodiepassenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RestaurantsNearMe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurants_near_me);
+    }
+}
