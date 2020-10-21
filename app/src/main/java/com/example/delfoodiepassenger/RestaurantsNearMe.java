8f302c60befc1a6d074b369ee083aa1ca9241f28
Richu Jain <richujain1995@gmail.com>
2020-10-21 10:07:02 -0400

    Implemented a listview that displays nearby restaurants using Places API.

--- app/src/main/java/com/example/delfoodiepassenger/RestaurantsNearMe.java
+++ app/src/main/java/com/example/delfoodiepassenger/RestaurantsNearMe.java
@@ -1,14 +1,111 @@
 package com.example.delfoodiepassenger;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class RestaurantsNearMe extends AppCompatActivity {
+import com.google.android.material.snackbar.Snackbar;
+
+public class RestaurantsNearMe extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
+
+    private static final int PERMISSION_REQUEST_LOCATION = 0;
+    private View mLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants_near_me);
+        mLayout = findViewById(R.id.layout);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showRestaurants();
+            }
+        });
+    }
+    public void showRestaurants(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            // Permission is already available, show restaurants
+            Snackbar.make(mLayout,
+                    "Location permission available. Show restaurants.",
+                    Snackbar.LENGTH_SHORT).show();
+            startRestaurants();
+        } else {
+            // Permission is missing and must be requested.
+            requestLocationPermission();
+        }
+    }
+    private void requestLocationPermission() {
+        // Permission has not been granted and must be requested.
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+            // Provide an additional rationale to the user if the permission was not granted
+            // and the user would benefit from additional context for the use of the permission.
+            // Display a SnackBar with a button to request the missing permission.
+            Snackbar.make(mLayout, "Location access is required to display restaurants near you.",
+                    Snackbar.LENGTH_INDEFINITE).setAction("OK", new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // Request the permission
+                    ActivityCompat.requestPermissions(RestaurantsNearMe.this,
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                            PERMISSION_REQUEST_LOCATION);
+                }
+            }).show();
+
+        } else {
+            Snackbar.make(mLayout,
+                    "Permission is not available. Requesting location permission.",
+                    Snackbar.LENGTH_SHORT).show();
+            // Request the permission. The result will be received in onRequestPermissionResult().
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSION_REQUEST_LOCATION);
+        }
+    }
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST_LOCATION) {
+            // Request for location permission.
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission has been granted. Start preview Activity.
+                Snackbar.make(mLayout, "Location permission granted. Showing restaurants.",
+                        Snackbar.LENGTH_SHORT)
+                        .show();
+                startRestaurants();
+            } else {
+                // Permission request was denied.
+                Snackbar.make(mLayout, "Location permission request was denied.",
+                        Snackbar.LENGTH_SHORT)
+                        .show();
+            }
+        }
+    }
+    public void startRestaurants() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED)
+        {
+            Intent intent = new Intent(this, DisplayRestaurants.class);
+            LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+            Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            Double longitude = location.getLongitude();
+            Double latitude = location.getLatitude();
+            String longit = Double.toString(longitude);
+            String lat = Double.toString(latitude);
+            intent.putExtra("long", longit);
+            intent.putExtra("lat", lat);
+            startActivity(intent);
+        }
     }
 }
